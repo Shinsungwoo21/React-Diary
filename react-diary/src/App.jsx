a6a9bc605ce657ccfc457b2ba63c945dc5c1d30a
Shinsungwoo21 <tlstjd1007@naver.com>
2025-03-19 20:58:11 +0900

    다이어리 리스트까지 완성

--- react-diary/src/App.jsx
+++ react-diary/src/App.jsx
@@ -4,10 +4,13 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
-import { useReducer, useRef } from "react";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 
 function reducer(state, action) {
   switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
     case "CREATE": {
       return [action.data, ...state];
     }
@@ -16,16 +19,51 @@ function reducer(state, action) {
         String(it.id) === String(action.data.id) ? {...action.data} : it
       );
     }
+    case "DELETE": {
+      return state.filter((it) => String(it.id) !== String(action.targetId));
+    }
     default: {
       return state;
     }
   }
 }
 
-function App() {
+const mockData = [
+  {
+    id: "mock1",
+    date: new Date().getTime() - 1,
+    content: "mock1",
+    emotionId: 1,
+  },
+  {
+    id: "mock2",
+    date: new Date().getTime() - 2,
+    content: "mock2",
+    emotionId: 2,
+  },
+  {
+    id: "mock3",
+    date: new Date().getTime() - 3,
+    content: "mock3",
+    emotionId: 3,
+  },
+]
 
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+function App() {
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
   const [data, dispatch] = useReducer(reducer, []);
   const idRef = useRef(0);
+
+  useEffect(() => {
+    dispatch({
+      type: "INIT",
+      data: mockData,
+    });
+    setIsDataLoaded(true);
+  }, []);
 
   const onCreate = (date, content, emotionId) => {
     dispatch({
@@ -52,17 +90,38 @@ function App() {
     });
   };
 
-  return (
-    <div className = "App">
-      <Routes>
-        <Route path = "/" element = {<Home />} />
-        <Route path = "/new" element = {<New />} />
-        <Route path = "/diary/:id" element = {<Diary />} />
-        <Route path = "/edit" element = {<Edit />} />
-      </Routes>
-    </div>
-  );
-      
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
+  };
+
+  if (!isDataLoaded) {
+    return <div>데이터를 불러오는 중입니다.</div>;
+  } else {
+    return (
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider
+          value={{
+            onCreate,
+            onUpdate,
+            onDelete,
+          }}
+        >
+          <div className = "App">
+            <Routes>
+              <Route path = "/" element = {<Home />} />
+              <Route path = "/new" element = {<New />} />
+              <Route path = "/diary/:id" element = {<Diary />} />
+              <Route path = "/edit/:id" element = {<Edit />} />
+            </Routes>
+          </div>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
+    );
+  }
+
 }
 
 export default App;
--- react-diary/src/pages/Home.jsx
+++ react-diary/src/pages/Home.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import Button from "../component/Button";
 import Header from "../component/Header";
 import Editor from "../component/Editor";
+import { DiaryStateContext } from '../App';
+import { getMonthRangeByDate } from '../util';
+import DiaryList from '../component/DiaryList';
 
 const Home = () => {
 
+  const data = useContext(DiaryStateContext);
+  const [pivotDate, setPivotDate] = useState(new Date());
+  const [filteredData, setFilterdData] = useState([]);
+  const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`;
+
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  }
+
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  }
+
+  useEffect(() => {
+    if (data.length >= 1) {
+      const { beginTimeStamp, endTimeStamp } = getMonthRangeByDate(pivotDate);
+      setFilterdData(
+        data.filter(
+          (it) => beginTimeStamp <= it.date && it.date <= endTimeStamp
+        )
+      );
+    } else {
+      setFilterdData([]);
+    }
+  }, [data, pivotDate]);
+
   return (
     <div>
-      Home 페이지입니다!
+      <Header
+        title={headerTitle}
+        leftChild={<Button text={"<"} onClick={onDecreaseMonth}/>}
+        rightChild={<Button text={">"} onClick={onIncreaseMonth}/>}
+      />
+      <DiaryList data={filteredData} />
     </div>
   )
 }
