bf9afa83fbd08dbfca8f2f119e5ffefab1a06e9f
Shinsungwoo21 <tlstjd1007@naver.com>
2025-03-21 20:54:57 +0900

    일기 작성 완료까지

--- react-diary/src/pages/Diary.jsx
+++ react-diary/src/pages/Diary.jsx
@@ -1,15 +1,41 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import useDiary from '../hooks/useDiary';
+import Button from '../component/Button';
+import Header from '../component/Header';
+import { getEmotionImgById, getFormattedDate } from '../util';
+import Viewer from '../component/Viewer';
 
 const Diary = () => {
 
   const { id } = useParams();
+  const data = useDiary(id);
+  const navigate = useNavigate();
 
-  return (
-    <div>
-      <p>{id}번 일기장입니다.</p>
-    </div>
-  )
-}
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+  
+  if (!data) {
+    return <div>일기를 불러오고 있습니다...</div>;
+  } else {
+    const { date, emotionId, content } = data;
+    const title = `${getFormattedDate(new Date(Number(date)))} 기록`;
+    return (
+      <div>
+        <Header
+          title={title}
+          leftChild={<Button text={"< 뒤로가기"} onClick={goBack} />}
+          rightChild={<Button text={"수정하기"} onClick={goEdit} />}
+        />
+        <Viewer content={content} emotionId={emotionId} />
+      </div>
+    );
+  }
+};
 
 export default Diary;
